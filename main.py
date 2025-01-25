ca665378b93fb6904010090033ec84d92ce80d06
Gabita101 <keren.gaby.ramirez@gmail.com>
2025-01-25 14:20:03 -0600

    Create main.py
    Homework of Kodland

---
+++ main.py
@@ -0,0 +1,18 @@
+meme_dict = {
+            "CRINGE": "Algo excepcionalmente raro o embarazoso",
+            "LOL": "Una respuesta común a algo gracioso",
+            'ROFL': 'una respuesta a una broma',
+            'SHEESH': 'ligera desaprobación',
+            'CREEPY': 'aterrador, siniestro',
+            'QUE ONDA': 'expresión mexicana que se usa para saludar o preguntar cómo está alguien',
+            'VIBES': 'Se usa para describir la atmósfera o sensación que algo transmite, tanto en personas como situaciones',
+            'BTW': 'significa por cierto o a propósito'
+            }
+
+while True:
+    word = input("Escribe una palabra que no entiendas (¡con mayúsculas!): ")
+    
+    if word in meme_dict.keys():
+        print(meme_dict[word])
+    else:
+        print('Lo siento esta palabra no se encuentra en el diccionario')
